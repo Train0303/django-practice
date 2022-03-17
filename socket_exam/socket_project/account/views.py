643ae586a5e606382011bcb98240d58cb9f2c71d
Train0303 <rjsdnxogh55@gmail.com>
2022-03-17 21:13:02 +0900

    modi signup

--- socket_exam/socket_project/account/views.py
+++ socket_exam/socket_project/account/views.py
@@ -29,15 +29,19 @@ def signup(request):
     if request.method == "POST":
         if request.POST.get('password1') == request.POST.get('password2'):
             try:
-                user = User.objects.create_user(email=request.POST.get('email'),
+                User.objects.get(email=request.POST.get('email'))
+                return render(request, 'account/signup.html') 
+            except:
+                try:
+                    User.objects.get(email=request.POST.get('username'))
+                    return render(request, 'account/signup.html') 
+                except:
+                    user = User.objects.create_user(email=request.POST.get('email'),
                                                 username=request.POST.get('username'),
                                                 password=request.POST.get('password1'))
-                auth_login(request,user)
-                print("생성완료")
-                return redirect("chat:index")
-            except:
-                
-                print("생성실패1")
-                return render(request, 'account/signup.html')            
+                    auth_login(request,user)
+                    print("생성완료")
+                    return redirect("chat:index")  
+                              
     print("생성실패2")
     return render(request, 'account/signup.html')
