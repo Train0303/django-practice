794ed4ff80f9fe827db6b3e970821c3e5c56f6e3
Train0303 <rjsdnxogh55@gmail.com>
2022-03-30 17:13:23 +0900

    add FBV CBV

--- rest_exam/django-rest/account/urls.py
+++ rest_exam/django-rest/account/urls.py
@@ -2,12 +2,18 @@ from django.conf import settings
 from django.urls import path,include
 from django.conf.urls.static import static
 from rest_framework import routers
-from .views import UserViewSet
+# from .views import UserViewSet
+# from .views import user_list_create_api_view,user_detail_api_view
+from .views import UserListAPIView,UserDetailAPIView
 
 router = routers.DefaultRouter()
-router.register(r'^user',UserViewSet)
+# router.register(r'^user',UserViewSet)
+# router.register(r'^user/',user_list_create_api_view,basename="userlist")
+# router.register(r'^user/<str:username>/',user_detail_api_view,basename="userdetail")
 
 urlpatterns = [
     path(r'^',include(router.urls)),
+    path(r"user",UserListAPIView.as_view()),
+    path(r"user/<str:username>",UserDetailAPIView.as_view()),
 ]
 urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
--- rest_exam/django-rest/account/views.py
+++ rest_exam/django-rest/account/views.py
@@ -1,20 +1,97 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from rest_framework import viewsets
 from rest_framework.response import Response
-
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from rest_framework import status
 from .models import User
 from .serializers import UserSerializer
 
 
+# @api_view(['GET','POST'])
+# def user_list_create_api_view(request):
+#     if (request.method) == "GET":
+#         users = User.objects.filter(is_active = True)
+#         serializer = UserSerializer(users,many=True)
+#         return Response(serializer.data)
+    
+#     elif (request.method) == "POST":
+#         serializer = UserSerializer(data = request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data,status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    def get_queryset(self):
-        qs = super().get_queryset()
-        search = self.request.query_params.get('search', '')
-        if search:
-            qs = qs.filter(username__contains=search)
+# @api_view(['GET','PUT','DELETE'])
+# def user_detail_api_view(request,username):
+#     try:
+#         user = User.objects.get(username=username)
+#     except User.DoesNotExist:
+#         return Response({"error":
+#         {
+#             "code" : 404,
+#             "message" : "User not found"
+#         }},status=status.HTTP_404_NOT_FOUND)
+    
+#     if request.method == "GET":
+#         serializer = UserSerializer(user)
+#         return Response(serializer.data)
+
+#     elif request.method == "PUT":
+#         serializer = UserSerializer(user,data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+#     elif request.method == "DELETE":
+#         user.delete()
+#         return Response(status = status.HTTP_204_NO_CONTENT)
+
+
+class UserListAPIView(APIView):
+    def get(self, request):
+        serializer = UserSerializer(User.objects.all(), many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = UserSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status = status.HTTP_201_CREATED)
+        return Response(serializer.errors,status = status.HTTP_400_BAD_REQUEST)
+    
+
+class UserDetailAPIView(APIView):
+    def get_object(self,username):
+        return get_object_or_404(User, username = username)
+
+    def get(self,request,username,format=None):
+        user = self.get_object(username)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+    
+    def put(self,request,username):
+        user = self.get_object(username)
+        serializer = UserSerializer(user)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self,request,username):
+        user = self.get_object(username)
+        user.delete()
+        return Response(stauts=status.HTTP_204_NO_CONTENT)
         
-        return qs
+# class UserViewSet(viewsets.ModelViewSet):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+#     def get_queryset(self):
+#         qs = super().get_queryset()
+#         search = self.request.query_params.get('search', '')
+#         if search:
+#             qs = qs.filter(username__contains=search)
+        
+#         return qs
 
